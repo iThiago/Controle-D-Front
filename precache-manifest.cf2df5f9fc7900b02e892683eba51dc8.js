e6747935bd6f7f1027ee4c9b3427cc402852c88b
Thiago Neto <thiagonetorj@hotmail.com>
2020-01-04 17:17:24 -0300

    Updates

---
+++ precache-manifest.cf2df5f9fc7900b02e892683eba51dc8.js
@@ -0,0 +1,33 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "053620169222566a01c51a432bd30147",
+    "url": "/ControleD-MASTER/index.html"
+  },
+  {
+    "url": "/ControleD-MASTER/precache-manifest.506ee35ec3f496ab37db40d3f6e3be44.js"
+  },
+  {
+    "revision": "01be0c4fced21c53ff4801015679e04a",
+    "url": "/ControleD-MASTER/service-worker.js"
+  },
+  {
+    "revision": "d506b83153093359c35b",
+    "url": "/ControleD-MASTER/static/css/main.04cf1866.chunk.css"
+  },
+  {
+    "revision": "7136a5eccc96f534d926",
+    "url": "/ControleD-MASTER/static/js/2.6d865366.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/ControleD-MASTER/static/js/2.6d865366.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d506b83153093359c35b",
+    "url": "/ControleD-MASTER/static/js/main.cae35395.chunk.js"
+  },
+  {
+    "revision": "cc95a1106209d9153369",
+    "url": "/ControleD-MASTER/static/js/runtime-main.ec7964d3.js"
+  }
+]);
